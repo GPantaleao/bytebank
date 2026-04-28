c9bad70436c1466ec9a453b8d3c7861a978e0808
Lucas Nery de Araujo <neryluc@gmail.com>
2026-04-28 09:57:35 -0300

    Adiciona pagina de transaferencias com componente Extrato (#4)

---
+++ src/app/transactions/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Extrato } from "@/app/components/organisms/Extrato";
+
+export default function TransactionsPage() {
+  return (
+    <div className="p-8">
+      <Extrato />
+    </div>
+  );
+}
